0ff11b8004f9f339df813b071076cab33bdfb468
Amirreza Madanifard <amadanifard@gmail.com>
2024-09-13 18:07:47 +0330

    Add questions to detail survey api

--- survey/serializers/survey_serializer.py
+++ survey/serializers/survey_serializer.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from survey.models.surveys import Surveys
+from survey.serializers.question_serializer import QuestionSerializer
 
 
 class SurveySerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True, read_only=True)
+
     class Meta:
         model = Surveys
         fields = ['id', 'user', 'title',
-                  'description', 'is_active', 'created_at']
+                  'description', 'is_active',
+                  'created_at', 'questions']
