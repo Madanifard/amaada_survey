fe3a315b2d16a72c6a641fcd9123d7712b2d67a6
Amirreza Madanifard <amadanifard@gmail.com>
2024-09-13 17:12:01 +0330

    add related_name to question ForeignKey

--- survey/models/options.py
+++ survey/models/options.py
@@ -3,6 +3,8 @@ from .questions import Questions
 
 
 class Options(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions,
+                                 on_delete=models.CASCADE,
+                                 related_name='options')
     key = models.CharField(max_length=256)
     value = models.JSONField()
